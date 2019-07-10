64efb93f6e79c662fe5acb14340a753a03342bb8
sg <srfngrc@outlook.com>
2019-07-10 18:27:40 -0400

    1st upload july10th 6h30pm

---
+++ Coffee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter08EXERCICES
+{
+    class Coffee : IHotDrink
+    {
+        public bool Instant { get; set; }
+        public bool Milk { get; set; }
+        public byte Sugar { get; set; }
+        public string Description { get; set; }
+
+        public byte AddSugar(byte quantity)
+        {
+            //Code that adds some sugar
+            //throw new NotImplementedException();
+            return quantity;
+        }
+    }
+}
---
+++ IHotDrink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter08EXERCICES
+{
+    interface IHotDrink
+    {
+        //Properties
+        bool Instant { get; set; }
+        bool Milk { get; set; }
+        byte Sugar { get; set; }
+        string Description { get; set; }
+        //Methods
+        byte AddSugar(byte quantity);
+    }
+}
